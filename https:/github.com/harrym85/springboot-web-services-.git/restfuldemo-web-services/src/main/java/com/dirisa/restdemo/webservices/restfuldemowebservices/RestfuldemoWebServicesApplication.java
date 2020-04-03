653160fe22bf099750f0b76808d38e4af42d42ec
thembhanimapengo <harrymapengo@gmail.com>
2020-04-04 01:18:35 +0200

    test

---
+++ https:/github.com/harrym85/springboot-web-services-.git/restfuldemo-web-services/src/main/java/com/dirisa/restdemo/webservices/restfuldemowebservices/RestfuldemoWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.dirisa.restdemo.webservices.restfuldemowebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+@SpringBootApplication
+public class RestfuldemoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfuldemoWebServicesApplication.class, args);
+	}
+
+}
---
+++ https:/github.com/harrym85/springboot-web-services-.git/restfuldemo-web-services/src/test/java/com/dirisa/restdemo/webservices/restfuldemowebservices/RestfuldemoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dirisa.restdemo.webservices.restfuldemowebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfuldemoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
